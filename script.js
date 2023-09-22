fe5a30278cfbc253727e850acd07d8d9e1ea9880
Killian Lecrut <killianlecrut@MacBook-Air-de-Killian.local>
2023-09-22 15:29:53 +0200

    good

--- script.js
+++ script.js
@@ -1,72 +1,73 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const projectCards = document.querySelectorAll(".project-card");
-    const modal = document.getElementById("modal");
-    const modalContent = modal.querySelector(".modal-content");
-    const modalName = modalContent.querySelector("h2");
-    const swiperWrapper = modal.querySelector(".swiper-wrapper");
-  
-    // Vérifier si l'appareil est tactile (événement "ontouchstart" pris en charge) 
-    // et si l'événement "mousedown" est également pris en charge
-    const isTouchDevice = "ontouchstart" in window || navigator.msMaxTouchPoints;
-    const isMouseAndTouchDevice = isTouchDevice && "onmousedown" in window;
-  
-    // Ajouter un gestionnaire d'événement clic à chaque carte de projet
-    projectCards.forEach((card) => {
-      card.addEventListener("click", function () {
-        const projectName = card.getAttribute("data-name");
-        const projectDescription = card.getAttribute("data-description");
-        const projectImages = card.getAttribute("data-images");
-  
-        // Mettre à jour le contenu de la modal avec le nom et la description du projet
-        modalName.textContent = projectName;
-        modalContent.querySelector("p").textContent = projectDescription;
-  
-        // Vider le contenu actuel du carrousel Swiper
-        swiperWrapper.innerHTML = "";
-  
-        // Vérifier si des images sont disponibles pour le projet
-        if (projectImages) {
-          const imageSrcArray = projectImages.split(",");
-          // Ajouter les images du carrousel en fonction du projet sélectionné
-          imageSrcArray.forEach((imageSrc) => {
-            const swiperSlide = document.createElement("div");
-            swiperSlide.className = "swiper-slide";
-            const image = document.createElement("img");
-            image.src = imageSrc;
-            image.alt = projectName;
-            swiperSlide.appendChild(image);
-            swiperWrapper.appendChild(swiperSlide);
-          });
-        }
-  
-        // Afficher la modal
-        modal.style.display = "block";
-  
-        // Initialisation du carrousel Swiper
-        const swiper = new Swiper(".swiper-container", {
-          navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-            disabledClass: isMouseAndTouchDevice ? "swiper-button-disabled" : "",
-          },
-          touchEventsTarget: isTouchDevice ? ".swiper-wrapper" : "auto",
+  const projectCards = document.querySelectorAll(".project-card");
+  const modal = document.getElementById("modal");
+  const modalContent = modal.querySelector(".modal-content");
+  const modalName = modalContent.querySelector("h2");
+  const swiperWrapper = modal.querySelector(".swiper-wrapper");
+
+  // Vérifier si l'appareil est tactile (événement "ontouchstart" pris en charge) 
+  // et si l'événement "mousedown" est également pris en charge
+  const isTouchDevice = "ontouchstart" in window || navigator.msMaxTouchPoints;
+  const isMouseAndTouchDevice = isTouchDevice && "onmousedown" in window;
+
+  // Ajouter un gestionnaire d'événement clic à chaque carte de projet
+  projectCards.forEach((card) => {
+    card.addEventListener("click", function () {
+      const projectName = card.getAttribute("data-name");
+      const projectDescription = card.getAttribute("data-description");
+      const projectImages = card.getAttribute("data-images");
+
+      // Mettre à jour le contenu de la modal avec le nom et la description du projet
+      modalName.textContent = projectName;
+      modalContent.querySelector("p").textContent = projectDescription;
+
+      // Vider le contenu actuel du carrousel Swiper
+      swiperWrapper.innerHTML = "";
+
+      // Vérifier si des images sont disponibles pour le projet
+      if (projectImages) {
+        const imageSrcArray = projectImages.split(",");
+        // Ajouter les images du carrousel en fonction du projet sélectionné
+        imageSrcArray.forEach((imageSrc) => {
+          const swiperSlide = document.createElement("div");
+          swiperSlide.className = "swiper-slide";
+          const image = document.createElement("img");
+          image.src = imageSrc;
+          image.alt = projectName;
+          swiperSlide.appendChild(image);
+          swiperWrapper.appendChild(swiperSlide);
         });
+      }
+
+      // Afficher la modal
+      modal.style.display = "block";
+
+      // Initialisation du carrousel Swiper
+      const swiper = new Swiper(".swiper-container", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+          disabledClass: isMouseAndTouchDevice ? "swiper-button-disabled" : "",
+        },
+        touchEventsTarget: isTouchDevice ? ".swiper-wrapper" : "auto",
+        slidesPerView: 1,
       });
     });
-  
-    // Gestionnaire d'événement pour fermer la modal
-    modal.querySelector(".close").addEventListener("click", function () {
-      modal.style.display = "none";
-    });
-  
-    // Gestionnaire d'événement pour fermer la modal lorsque l'utilisateur clique en dehors de celle-ci
-    modal.addEventListener("click", function (event) {
-      if (event.target === modal) {
-        modal.style.display = "none";
-      }
-    });
   });
-  
+
+  // Gestionnaire d'événement pour fermer la modal
+  modal.querySelector(".close").addEventListener("click", function () {
+    modal.style.display = "none";
+  });
+
+  // Gestionnaire d'événement pour fermer la modal lorsque l'utilisateur clique en dehors de celle-ci
+  modal.addEventListener("click", function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+});
+
 
 
 
